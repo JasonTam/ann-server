e6697bbc14755647bc58db3c5d19d5f7eee6a743
jason <JasonTam22@gmail.com>
2019-04-04 09:34:11 -0400

    targz as argument to server

--- run_server.sh
+++ run_server.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-PATH_INDEX='s3://jason-garbage/ann/glove100/glove100.ann'
-PATH_IDS='s3://jason-garbage/ann/glove100/glove100_ids'
-N_DIM=100
-
-#PATH_INDEX='s3://jason-garbage/ann/glove1m/glove1m.ann'
-#PATH_IDS='s3://jason-garbage/ann/glove1m/glove1m_ids'
-#N_DIM=25
+PATH_TAR='s3://mo-ml-dev/ann/dresses.tar.gz'
 
 gunicorn \
     -k gevent \
     -b 0.0.0.0:8000 \
-    "api.app_falcon:build_app('$PATH_INDEX', '$PATH_IDS', $N_DIM)"
+    "api.app_falcon:build_app('$PATH_TAR')"
