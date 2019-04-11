ae05dabb1ebc0fea4b4454b4662552b42d54638a
jason <JasonTam22@gmail.com>
2019-04-11 10:51:30 -0400

    oops forgot to add

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MODE=${1:-"many"}  # { "many" | "single" }
+PATH_ANN=${2:-"s3://mo-ml-dev/ann/"}
+
+
+if [ $MODE == "many" ]
+then
+    APP_FN="api.app_falcon:build_many_app('$PATH_ANN')"
+else
+    APP_FN="api.app_falcon:build_single_app('$PATH_ANN')"
+fi
+
+gunicorn \
+    -k gevent \
+    -b 0.0.0.0:8000 \
+    $APP_FN
