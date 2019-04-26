9f13cfa2101d08788a0b246725b83f872546dd67
Jason Tam <JasonTam22@gmail.com>
2019-04-26 17:49:15 -0400

    cross ANN lookup

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -1,3 +1,4 @@
 from app.resources.ann import ANNResource
+from app.resources.cross import CrossANNResource
 from app.resources.refresh import RefreshResource, MaybeRefreshAllResource
 from app.resources.health import ANNHealthcheckResource, HealthcheckResource
