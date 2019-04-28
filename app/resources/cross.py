862a259a196c66ff42309e703bc33ca714fff78b
Jason Tam <JasonTam22@gmail.com>
2019-04-28 01:34:13 -0400

    incl dist on cross

--- app/resources/cross.py
+++ app/resources/cross.py
@@ -19,9 +19,11 @@ class CrossANNResource(object):
         q_id = req.params['q_id']
         c_name = req.params['catalog_name']
         k = int(req.params['k'])
+        incl_dist = bool(req.params.get('incl_dist')) or False
 
         q_emb = self.ann_resources_d[q_name].get_vector(q_id)
-        neighbors = self.ann_resources_d[c_name].nn_from_emb(q_emb, k)
+        neighbors = self.ann_resources_d[c_name].nn_from_emb(
+                q_emb, k, incl_dist=incl_dist)
 
         if neighbors is None:
             resp.status = falcon.HTTP_200
