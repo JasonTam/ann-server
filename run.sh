60d78bea4cdebe1f0191f515b84e17ac3437dda0
Jason Tam <JasonTam22@gmail.com>
2019-04-29 13:55:29 -0400

    fix desc

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+PATH_ANN=${1:-"s3://mo-ml-dev/ann/"}
+OOI_TABLE=${2:-"img-reprs"}
+PATH_FALLBACK=${3:-"s3://mo-ml-dev/subcat_parent_map.json"}
+CHECK_INTERVAL=${4:-3600}
+
+
+APP_FN="app_builder:build_many_app("\
+"'$PATH_ANN',"\
+"'$OOI_TABLE',"\
+"'$PATH_FALLBACK',"\
+"$CHECK_INTERVAL,"\
+")"
+
+
+gunicorn \
+    --timeout 90 \
+    -k gevent \
+    -b 0.0.0.0:8000 \
+    ${APP_FN}
